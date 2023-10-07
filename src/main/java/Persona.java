d8acea5c6faf801e6175f679c43eede45f63481c
javier <j.alcalde01@ufromail.cl>
2023-10-07 20:19:38 -0300

    quinto commit

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -2,20 +2,19 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Persona {
-
 	private String nombre;
 
 	public static Scanner scanner = new Scanner(System.in);
 	public void setNombre(String nombre){
 		this.nombre = nombre;
 	}
-	public int validarInt(){
+	public float validarInt(){
 		int distancia = 0;
 		while (true) {
 			try {
-				System.out.println("Introduzca la distancia de su destino (debe ser un número entero):");
+				System.out.println("Introduzca la distancia de su destino en km: ");
 				distancia = scanner.nextInt();
-				break; // Si se ingresa un valor válido, sal del bucle
+				break;
 			} catch (InputMismatchException e) {
 				System.err.println("Error: Debe ingresar un número entero válido.");
 				scanner.nextLine(); // Limpia el búfer de entrada
@@ -30,60 +29,54 @@ public class Persona {
 	}
 	public String introducirNombreDestino(){
 		System.out.println("Introduzca el nombre de su destino: ");
-		String nombreDestino;
-		return nombreDestino = scanner.next();
+		return scanner.next();
 	}
 	public void viajar() {
-		if(elegirVehiculo() == 1){
+		int eleccion = elegirVehiculo();
+		if(eleccion == 1){
 			Auto auto = new Auto();
 			introducirNombrePersona();
 			String nombreDestino = introducirNombreDestino();
-			int distanciaDestino = validarInt();
+			float distanciaDestino = validarInt();
 			float tiempoEnLlegar = auto.tiempoEnLlegar(distanciaDestino);
-			System.out.println("Hola " + this.nombre + " el tiempo en llegar a " + nombreDestino + " en " +
-					auto.nombre + " es de " + tiempoEnLlegar + " minutos");
-		}else if(elegirVehiculo() == 2){
+			System.out.println("Hola " + this.nombre + "! el tiempo en llegar a " + nombreDestino + " en " +
+					auto.nombre + " es de " + tiempoEnLlegar + " horas");
+		}else if(eleccion == 2){
 			Motocicleta moto = new Motocicleta();
 			introducirNombrePersona();
 			String nombreDestino = introducirNombreDestino();
-			int distanciaDestino = validarInt();
+			float distanciaDestino = validarInt();
 			float tiempoEnLlegar = moto.tiempoEnLlegar(distanciaDestino);
-			System.out.println("Hola " + this.nombre + " el tiempo en llegar a " + nombreDestino + " en " +
-					moto.nombre + " es de " + tiempoEnLlegar + " minutos");
-		}else if(elegirVehiculo() == 3){
+			System.out.println("Hola " + this.nombre + "! el tiempo en llegar a " + nombreDestino + " en " +
+					moto.nombre + " es de " + tiempoEnLlegar + " horas");
+		}else if(eleccion == 3){
 			Bicicleta bici = new Bicicleta();
 			introducirNombrePersona();
 			String nombreDestino = introducirNombreDestino();
-			int distanciaDestino = validarInt();
+			float distanciaDestino = validarInt();
 			float tiempoEnLlegar = bici.tiempoEnLlegar(distanciaDestino);
-			System.out.println("Hola " + this.nombre + " el tiempo en llegar a " + nombreDestino + " en " +
-					bici.nombre + " es de " + tiempoEnLlegar + " minutos");
+			System.out.println("Hola " + this.nombre + "! el tiempo en llegar a " + nombreDestino + " en " +
+					bici.nombre + " es de " + tiempoEnLlegar + " horas");
 		}else {
 			System.out.println("Algo salió mal");
 		}
 	}
 	public int elegirVehiculo(){
-		int opcion;
-		int seleccion = 0;
+		int opcion = 0;
+		int eleccion = 0;
 		do {
 			System.out.println("Qué vehiculo desea conducir? Escoja una opción: ");
-			System.out.println("[1] Auto");
-			System.out.println("[2] Motocicleta");
-			System.out.println("[3] Bicicleta");
+			System.out.println("[1] Auto (60 km)");
+			System.out.println("[2] Motocicleta (50 km)");
+			System.out.println("[3] Bicicleta (20 km)");
 			opcion = scanner.nextInt();
-			switch (opcion){
-				case 1 -> {
-					seleccion = 1;
-				}
-				case 2 -> {
-					seleccion = 2;
-				}
-				case 3 -> {
-					seleccion = 3;
-				}
-				default -> System.out.println("Intente con una opción valida: ");
+			if (opcion >=1 && opcion <=3){
+				eleccion = opcion;
+				break;
+			}else {
+				System.out.println("Ingrese una opcion valida");
 			}
-		}while (seleccion < 1 && seleccion > 3);
-		return seleccion;
+		}while (true);
+		return eleccion;
 	}
 }
